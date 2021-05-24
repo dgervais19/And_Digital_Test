b468a6d049705e9c00b974965552d81f70f592ca
Donovan Gervais <applemac@Donovans-MacBook-Pro-139.local>
2021-05-24 16:58:35 +0100

    completed VPC and ALB

--- Network.tf
+++ Network.tf
@@ -6,20 +6,82 @@ resource "aws_vpc" "main" {
   }
 }
 
-# resource "aws_subnet" "PublicSubnet" {
-#     vpc_id = aws_vpc.main.id
-#     cidr_block = "19.6.1.0/32"
-#     tags = {
-#       "name" = "Public Subnet"
-#     }
-#     availability_zone = {"eu-west-1a", "eu-west-1b", "eu-west-1c"}
-# }
+resource "aws_subnet" "PublicSubnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "19.6.1.0/32"
+    tags = {
+      "name" = "Public Subnet"
+    }
+    availability_zone = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
 
-# resource "aws_subnet" "PrivateSubnet" {
-#     vpc_id = aws_vpc.main.id
-#     cidr_block = "19.6.2.0/32"
-#     tags = {
-#       "name" = "Private Subnet"
-#     }
-#     availability_zone = {"eu-west-1a", "eu-west-1b", "eu-west-1c"}
-# }
+resource "aws_subnet" "PrivateSubnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "19.6.2.0/32"
+    tags = {
+      "name" = "Private Subnet"
+    }
+    availability_zone = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+resource "aws_route_table_association" "PublicSubnet" {
+    subnet_id = "${aws_subnet.PublicSubnet.id}"
+    route_table_id = "${aws_route_table.public_route.id}"
+}
+
+resource "aws_route_table_association" "PrivateSubnet" {
+    subnet_id = "${aws_subnet.PrivateSubnet.id}"
+    route_table_id = "${aws_route_table.private_route.id}"
+}
+
+resource "aws_internet_gateway" "gw" {
+   vpc_id = "${aws_vpc.main.id}"
+    tags {
+        Name = "Internet Gateway"
+    }
+}
+
+resource "aws_network_acl" "access_to_all" {
+   vpc_id = "${aws_vpc.main.id}"
+    egress {
+        protocol = "-1"
+        rule_no = 2
+        action = "allow"
+        cidr_block =  "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
+    }
+    ingress {
+        protocol = "-1"
+        rule_no = 1
+        action = "allow"
+        cidr_block =  "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
+    }
+    tags {
+        Name = "open to all"
+    }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+      Name = "Public Route"
+  }
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+      Name = "Private Route"
+  }
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
--- redirect.tf
+++ redirect.tf
@@ -37,7 +37,7 @@ resource "aws_autoscaling_group" "scalegroup" {
   health_check_type     = "ELB"
   availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 
-  # target_group_arns = [aws_alb_target_group.http.arn]
+  target_group_arns = [aws_alb_target_group.http.arn]
 
   lifecycle {
     create_before_destroy = true
@@ -45,34 +45,56 @@ resource "aws_autoscaling_group" "scalegroup" {
 
 }
 
-
 # ALB  (Automatic Load Balance)
-# resource "aws_alb" "web" {
-#   name               = "web-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb.id]
+resource "aws_alb" "web" {
+  name               = "web-alb"
+  subnets            = ["${aws_subnet.PublicSubnet.id}", "${aws_subnet.PrivateSubnet.id}"]
+  security_groups    = ["${aws_security_group.alb_sec.id}"]
+  internal           = false
+  load_balancer_type = "application"
 
-#   enable_deletion_protection = false
+  enable_deletion_protection = false
+  tags = {
+    Name = "alb"
+  }
 
-# }
-
-# resource "aws_alb_listener" "https_listener" {
-#   load_balancer_arn = aws_alb.web.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.http.arn
-#   }
-# }
-
+}
 
 resource "aws_alb_target_group" "http" {
   name     = "web-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  tags {
+      name = "alb_target_group"
+  }
+stickiness {    
+    type            = "lb_cookie"    
+    cookie_duration = 1800    
+    enabled         = true 
+  }   
+  health_check {    
+    healthy_threshold   = 3    
+    unhealthy_threshold = 10    
+    timeout             = 5    
+    interval            = 10    
+    path                = "/"    
+    port                = 80
+  }
 }
 
+resource "aws_alb_listener" "https_listener" {
+  load_balancer_arn = aws_alb.web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.http.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "alb_autoscale" {
+  alb_target_group_arn   = "${aws_alb_target_group.http.arn}"
+  autoscaling_group_name = "${aws_autoscaling_group.scalegroup.id}"
+}
